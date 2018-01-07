03338e5796fd360e23bf5009d415102b1ab5a28b
shetuchowdhury17 <shetuchowdhury17@gmail.com>
2018-01-07 13:22:53 +0600

    new blog file create

---
+++ blog/app/classes/Login.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\classes;
+
+use App\classes\Database;
+class Login
+{
+    public function adminLoginCheck($data)
+    {
+        $email = $data ['email'];
+        $password = md5($data ['password']);
+       $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password' ";
+       if(mysqli_query(Database::dbConnection(), $sql))
+       {
+           $queryResult = mysqli_query(Database::dbConnection(), $sql);
+           $user = mysqli_fetch_assoc($queryResult);
+//           echo '<pre>';
+//           print_r($user);
+           if ($user)
+           {
+               header('Location: dashboard.php');
+           }
+           else
+           {
+               $message = "Please use valid email address & password";
+               return $message;
+           }
+       }
+       else
+       {
+           die("Query problem".mysqli_error(Database::dbConnection()));
+       }
+    }
+}
